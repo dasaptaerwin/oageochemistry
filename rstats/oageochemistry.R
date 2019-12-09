1a1854976fb7b5d6041d623e0665a438f1550d78
dasaptaerwin <dasaptaerwin@outlook.co.id>
2019-12-10 05:18:37 +0700

    adding code to rstats

---
+++ rstats/oageochemistry.R
@@ -0,0 +1,23 @@
+# install library (lakukan bila belum pernah menginstalasi)
+
+install.packages("tidyverse")
+
+# load library
+
+library(tidyverse)
+
+# load data
+
+## memeriksa data 
+
+
+
+
+# analisis
+
+
+
+## statistik dasar
+
+
+## korelasi
