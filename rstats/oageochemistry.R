512f3320daf3e52b0635a09730f530318ad44f16
dasaptaerwin <dasaptaerwin@outlook.co.id>
2019-12-12 05:38:07 +0700

    editing data xlsx

--- rstats/oageochemistry.R
+++ rstats/oageochemistry.R
@@ -9,8 +9,8 @@ install.packages("readxl")
 
 # load library
 
-library(tidyverse)
-library(readxl)
+library(tidyverse) # package untuk analisis dan visualisasi data
+library(readxl)    # package untuk membuka data format xls
 
 # load data
 
@@ -18,9 +18,12 @@ data <- read_excel("data.xlsx")
 
 ## memeriksa data 
 
-dim(data) # dimensi data 
-
-
+dim(data)       # dimensi data 
+names(data)     # nama-nama kolom
+head(data, n=5) # data 5 baris teratas
+tail(data, n=5) # data 5 baris terbawah
+str (data)      # struktur data, jenis kolom
+summary(data)   # ringkasan data
 
 # analisis
 
